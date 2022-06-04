28ac448dc1455fcc670d592d3004e75602911834
NguyenTrongAnh2002 <104582193+NguyenTrongAnh2002@users.noreply.github.com>
2022-06-04 12:39:03 +0700

    Update cau3.cpp

--- bài15/cau3.cpp
+++ bài15/cau3.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 void sap_tang_dan(int a[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 1; i <= n ; i++)
     {
-        for (int j = i + 1; j < n; j++)
+        for (int j = i + 1; j <= n; j++)
         {
             if (a[i] > a[j])
             {
@@ -25,15 +25,29 @@ void gready(int a[], int n, long s)
         {
             so_goi_hang_duoc_chon = s / a[i];
             s -= so_goi_hang_duoc_chon * a[i];
-            cout << "chon " << so_goi_hang_duoc_chon << " goi hang thu " << a[i]  << endl;
+            cout << "chon " << so_goi_hang_duoc_chon << " goi hang co thoi gian van chuyen la " << a[i]  << endl;
         }
     }
 }
+
 int main()
 {
     int a[10];
     int n;
 
+    do
+    {
+        cout << "nhap so luong goi hang: ";
+        cin >> n;
+    } while (n < 0);
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "nhap thoi gian goi hang " << i << ": ";
+        cin >> a[i];
+    }
+    sap_tang_dan(a, n);
+    gready(a, n, 10);
+}
     do
     {
         cout << "nhap so luong goi hang: ";
